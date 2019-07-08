0e6024c4145d916c860b8fac8b77be30cc460fbd
James McMahon <j.mcmahon1@nhs.net>
2019-07-08 17:00:47 +0100

    Adding files for talk

---
+++ GCC_10_06_19/dbplyr lightning talk/01 set up.R
@@ -0,0 +1,14 @@
+#  Load Libraries
+library(odbc)
+library(dplyr) # We only need to load dplyr
+library(magrittr)
+
+# Create a connection to SMRA
+SMRA_connection <- odbc::dbConnect(
+  drv = odbc::odbc(),
+  dsn = "SMRA",
+  uid = rstudioapi::showPrompt(title = "Username", message = "Username:"),
+  pwd = rstudioapi::askForPassword("SMRA Password:"),
+  encoding = "ASCII"
+)
+
